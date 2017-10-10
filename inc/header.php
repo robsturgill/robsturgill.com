e5ac63b07aa86e482211d48f251220f399a36d70
Rob Sturgill <rsturgill@jrthompson.com>
2017-10-09 21:27:51 -0400

    includes refactored

--- inc/header.php
+++ inc/header.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <title>Rob Sturgill - UX Director • Strategy • Design • Development</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Perfect masonry isotope portfolio template. Masonry portfolio examples. Build your portfolio with berger template." />
+  <meta name="keywords" content="masonry portfolio, portfolio design, portfolio template, minimal portfolio, masonry isotope portfolio " />
+  <meta name="author" content="ClaPat Studio">
+  <meta charset="UTF-8" />
 
-    <title>Rob Sturgill - UX Director • Strategy • Design • Development</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Perfect masonry isotope portfolio template. Masonry portfolio examples. Build your portfolio with berger template." />
-    <meta name="keywords" content="masonry portfolio, portfolio design, portfolio template, minimal portfolio, masonry isotope portfolio " />
-    <meta name="author" content="ClaPat Studio">
-    <meta charset="UTF-8" />
+  <link rel="icon" type="image/ico" href="favicon.ico" />
+  <link href="style.css" rel="stylesheet" />
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:500,600" rel="stylesheet">
 
-    <link rel="icon" type="image/ico" href="favicon.ico" />
-    <link href="style.css" rel="stylesheet" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:500,600" rel="stylesheet">
+  <!--
+   For efficiency sake, this was a purchased theme that has been extended for my needs.
+
+   xxxxx    xxxxx   xxxxx
+   xx   x  xx   xx  xx   x
+   xx  xx  xx   xx  xx xx
+   xx xx   xx   xx  xx   x
+   xx  xx   xxxxx   xx xx
+
+
+    xxxx xxxxxx  xx   xx  xxxxx    xxxx   xx  xx    xx
+   xx      xx    xx   xx  xx   x  xx      xx  xx    xx
+     xx    xx    xx   xx  xx  xx  xx  xx  xx  xx    xx
+      xx   xx    xx   xx  xx xx   xx   x  xx  xx    xx
+   xxxx    xx     xxxxx   xx  xx   xxxxx  xx  xxxx  xxxx
+
+  -->
 
 </head>
--- index.php
+++ index.php
@@ -20,56 +20,8 @@
             <div class="animate-box-bottom"></div>
         </div>
 
-        <!-- Header -->
-        <header class="scroll-hide">
-            <div id="header-container">
 
-            <!-- Logo -->
-            <div id="logo">
-                <a class="ajax-link" data-type="page-transition" href="index.html">
-                    <img class="black-logo" src="images/logo.png" alt="Logo Black">
-                    <img class="white-logo" src="images/logo-white.png" alt="Logo White">
-                </a>
-            </div>
-            <!--/Logo -->
-
-            <!-- Navigation -->
-            <nav>
-
-                <ul data-breakpoint="1024" class="flexnav">
-                    <li><a class="active" href="">Portfolio</a>
-                      <ul>
-                        <li><a class="ajax-link active" href="index.html" data-type="page-transition">Brick Wall</a></li>
-                        <li><a class="ajax-link" href="portfolio01.html" data-type="page-transition">Half Squares</a></li>
-                        <li><a class="ajax-link" href="portfolio02.html" data-type="page-transition">White Canvas</a></li>
-                        <li><a class="ajax-link" href="portfolio03.html" data-type="page-transition">Vertical Slides</a></li>
-                        <li><a class="ajax-link" href="portfolio04.html" data-type="page-transition">Classic Grid</a></li>
-                      </ul>
-                    </li>
-
-                    <li><a class="ajax-link" href="about.html" data-type="page-transition">About</a></li>
-
-                    <li><a class="ajax-link" href="blog.html" data-type="page-transition">Blog</a></li>
-
-                    <li><a class="ajax-link" href="contact.html" data-type="page-transition">Contact</a></li>
-
-                </ul>
-
-            </nav>
-            <!--/Navigation -->
-
-            <!-- Menu Burger -->
-            <div id="menu-burger">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <!--/Menu Burger -->
-
-            </div>
-        </header>
-        <!--/Header -->
+        <?php include_once('inc/navigation.php'); ?>
 
 
         <!-- Portfolio Filters -->
@@ -275,29 +227,4 @@
                 <div id="open-filters"><i class="fa fa-eye" aria-hidden="true"></i></div>
             </div>
 
-	    <?php include_once('inc/page-progress.php'); ?>
-
-
-        </footer>
-        <!--/Footer -->
-
-
-	</div>
-	<!--/Page Content -->
-
-    </div>
-</main>
-
-
-    <div class="cd-cover-layer"></div>
-
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/scripts.js"></script>
-
-
-
-</body>
-
-</html>
+        <?php include_once('inc/footer.php'); ?>

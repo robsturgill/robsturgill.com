00a1c70775602952c30b9b9887059eb748b20608
Rob Sturgill <rsturgill@jrthompson.com>
2017-10-11 11:26:11 -0400

    no message

--- automobility.php
+++ automobility.php
@@ -66,7 +66,7 @@
 
               <div class="container">
 
-                <img src="images/projects/solave01.gif" alt="">
+                <img src="/assets/img/projects/automobility/automobility-wireframes.jpg" alt="Automobility Wireframes">
 
                 <hr><hr>
 

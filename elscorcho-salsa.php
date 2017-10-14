3bb7ae5c0865f6dabccc59ec3b616f09051b3196
Rob Sturgill <rsturgill@jrthompson.com>
2017-10-14 16:17:19 -0400

    elscorcho project added

---
+++ elscorcho-salsa.php
@@ -0,0 +1,82 @@
+<?php
+  $title = 'El Scorcho Salsa • Motion Graphics';
+  $description = 'Motion graphics piece created for a class project.';
+?>
+<?php include_once('inc/header.php'); ?>
+
+<body class="hidden">
+  <main>
+    <div class="cd-index cd-main-content">
+
+      <!-- Page Content -->
+      <div id="page-content" class="light-content" data-bgcolor="#151515" data-textcolor="#fff">
+
+        <div class="page-overlay from-bottom">
+          <div class="animate-box-bottom"></div>
+        </div>
+
+        <?php include_once('inc/navigation.php'); ?>
+
+        <!-- Main -->
+        <div id="main">
+          <!-- Hero Section -->
+          <div id="hero">
+            <div id="hero-styles" class="parallax-onscroll opacity-onscroll">
+              <div id="hero-caption">
+                <div class="inner text-align-center">
+                  <h1 class="hero-title">El Scorcho Salsa</h1>
+                  <div class="hero-subtitle">Motion Graphics</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="hero-height" class="hidden"></div>
+          <!--/Hero Section -->
+
+          <div id="hero-image" class="hidden" style="background-image:url(/assets/img/projects/elscorcho-salsa/elscorcho-salsa-hero.jpg)">
+            <div id="image-border-left"></div>
+            <div id="image-border-right"></div>
+          </div>
+
+          <!-- Main Content -->
+          <div id="main-content">
+            <!-- Row -->
+            <div class="vc_row has-animation row_padding_top row_padding_bottom animate-in" data-delay="10">
+              <div class="container">
+                <div class=" one_third"><h2 style="color: rgb(255, 255, 255);">After Effect Experiment</h2></div>
+                <div class=" two_third last">
+                  <p>This was one of my first experimental shorts created in After Effects back in 2006. Entire video was made from a series of still images and particle systems.</p>
+                </div>
+              </div>
+
+              <div class="container">
+                <!-- 16:9 aspect ratio -->
+                <div class="embed-responsive embed-responsive-4by3">
+                  <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/8791391" width="640" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>
+
+
+            <div id="project-nav" class="has-animation" data-delay="100">
+              <div class="nav-title">
+                <a  class="ajax-link project-next" data-type="page-transition" href="/hitman.php">next</a>
+              </div>
+              <div class="nav-project-title"><span>Hitman Poster</span></div>
+            </div>
+
+
+
+          </div>
+          <!--/Main Content -->
+        </div>
+        <!--/Main -->
+
+
+        <!-- Footer -->
+        <footer class="hidden">
+          <a id="page-action-holder" class="ajax-link" href="/" data-type="page-transition" data-tooltip="Back to Works" data-placement="right">
+            <div id="backtoworks"><i class="fa fa-times" aria-hidden="true"></i></div>
+          </a>
+
+<?php include_once('inc/footer.php'); ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-  $title = 'Rob Sturgill - UX Director • Strategy • Design • Development';
-  $description = 'Creating lasting impressions though inspiration so people together, can create better experiences.';
+$title = 'Rob Sturgill - UX Director • Strategy • Design • Development';
+$description = 'Creating lasting impressions though inspiration so people together, can create better experiences.';
 ?>
 <?php include_once('inc/header.php'); ?>
 
@@ -12,16 +12,16 @@
 
 <body class="hidden">
 
-	<main>
+  <main>
 
 
-	<div class="cd-index cd-main-content">
+    <div class="cd-index cd-main-content">
 
-	<!-- Page Content -->
-	<div id="page-content" class="" data-bgcolor="#fff" data-textcolor="#000" data-portfoliocolor="#ccc">
+      <!-- Page Content -->
+      <div id="page-content" class="" data-bgcolor="#fff" data-textcolor="#000" data-portfoliocolor="#ccc">
 
         <div class="page-overlay from-bottom">
-            <div class="animate-box-bottom"></div>
+          <div class="animate-box-bottom"></div>
         </div>
 
 
@@ -30,17 +30,17 @@
 
         <!-- Portfolio Filters -->
         <div class="page-action-overlay">
-            <div class="outer">
-                <div class="inner">
-                    <div class="close-page-action"></div>
-                    <ul id="filters">
-                        <li><a id="all" href="#" data-filter="*" class="active">All</a><span>07</span></li>
-                        <li><a href="#" data-filter=".branding">Branding</a><span>03</span></li>
-                        <li><a href="#" data-filter=".design">Design</a><span>03</span></li>
-                        <li><a href="#" data-filter=".web">Web</a><span>01</span></li>
-                    </ul>
-                </div>
+          <div class="outer">
+            <div class="inner">
+              <div class="close-page-action"></div>
+              <ul id="filters">
+                <li><a id="all" href="#" data-filter="*" class="active">All</a><span>07</span></li>
+                <li><a href="#" data-filter=".branding">Branding</a><span>03</span></li>
+                <li><a href="#" data-filter=".design">Design</a><span>03</span></li>
+                <li><a href="#" data-filter=".web">Web</a><span>01</span></li>
+              </ul>
             </div>
+          </div>
         </div>
         <!--/Portfolio Filters -->
 
@@ -49,176 +49,176 @@
         <div id="main">
 
 
-            <!-- Hero Section -->
-            <div id="hero">
-                <div id="hero-styles" class="scale-onscroll opacity-onscroll">
-                    <div id="hero-caption">
-                        <div class="inner text-align-center">
-                            <div class="hero-title">Rob Sturgill / UX Professional</div>
-                            <div class="hero-subtitle">Creating lasting impressions though inspiration so people together, can create better experiences.</div>
+          <!-- Hero Section -->
+          <div id="hero">
+            <div id="hero-styles" class="scale-onscroll opacity-onscroll">
+              <div id="hero-caption">
+                <div class="inner text-align-center">
+                  <div class="hero-title">Rob Sturgill / UX Professional</div>
+                  <div class="hero-subtitle">Creating lasting impressions though inspiration so people together, can create better experiences.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="hero-height" class="hidden"></div>
+          <!--/Hero Section -->
+
+
+          <!-- Main Content -->
+          <div id="main-content">
+
+
+            <!-- Portfolio Wrap -->
+            <div id="portfolio-wrap" class="">
+
+              <!-- Portfolio Columns -->
+              <div id="portfolio" data-col="3">
+
+                <div class="item ux">
+                  <a href="automobility.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/automobility/automobility-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">FCA Automobility</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">UX/UI</div>
+                            <div class="item-case">See Case Study</div>
+                          </div>
                         </div>
+                      </div>
                     </div>
+                  </a>
                 </div>
-            </div>
-            <div id="hero-height" class="hidden"></div>
-            <!--/Hero Section -->
 
-
-            <!-- Main Content -->
-            <div id="main-content">
-
-
-                <!-- Portfolio Wrap -->
-                <div id="portfolio-wrap" class="">
-
-                    <!-- Portfolio Columns -->
-                    <div id="portfolio" data-col="3">
-
-                  	<div class="item ux">
-                    	<a href="automobility.php" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="/assets/img/projects/automobility/automobility-thumb.jpg"></div>
-                                <div class="item-caption">
-                                	<div class="item-overlay">
-                                        <div class="item-title">FCA Automobility</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">UX/UI</div>
-                                            <div class="item-case">See Case Study</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
+                <div class="item wide design">
+                  <a href="reed-modified.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/reed-modified/reed-modified-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Reed Modified</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Logo</div>
+                            <div class="item-case">View Detail</div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-
-                    <div class="item wide design">
-                    	<a href="reed-modified.php" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="/assets/img/projects/reed-modified/reed-modified-thumb.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Reed Modified</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Logo</div>
-                                            <div class="item-case">View Detail</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="item wide design">
-                        <a href="drpepper.php" class="ajax-link" data-type="page-transition">
-                            <div class="item-content">
-                                <div class="item-image" data-src="/assets/img/projects/drpepper/destiny-drpepper-thumb.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">First Sip</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Art Composition</div>
-                                            <div class="item-case">View Detail</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="item design">
-                    	<a href="hitman.php" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="/assets/img/projects/hitman/hitman-thumb.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Hitman Poster</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Art Composition</div>
-                                            <div class="item-case">View Details</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-			            </a>
-                    </div>
-
-                    <div class="item design">
-                    	<a href="project05.html" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="images/5.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Logo Animals</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Art Direction</div>
-                                            <div class="item-case">See Case Study</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-			</a>
-                    </div>
-
-                    <div class="item wide web">
-                    	<a href="project06.html" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="images/6.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Lanterna Pictures</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Web Design</div>
-                                            <div class="item-case">See Case Study</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-			</a>
-                    </div>
-
-                    <div class="item wide branding">
-                    	<a href="project07.html" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="images/7.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Bruni Fashion</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Branding</div>
-                                            <div class="item-case">See Case Study</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-			</a>
-                    </div>
-
-                    <div class="item design">
-                    	<a href="project08.html" class="ajax-link" data-type="page-transition">
-                    		<div class="item-content">
-                            	<div class="item-image" data-src="images/8.jpg"></div>
-                                <div class="item-caption">
-                                    <div class="item-overlay">
-                                        <div class="item-title">Yomagick</div>
-                                        <div class="item-sub-mask">
-                                            <div class="item-cat">Art Direction</div>
-                                            <div class="item-case">See Case Study</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-			</a>
-                    </div>
-
-
-                  </div>
-                  <!--/Portfolio -->
-
+                  </a>
                 </div>
-                <!--/Portfolio Wrap -->
 
+                <div class="item wide design">
+                  <a href="drpepper.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/drpepper/destiny-drpepper-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">First Sip</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Art Composition</div>
+                            <div class="item-case">View Detail</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="item design">
+                  <a href="hitman.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/hitman/hitman-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Hitman Poster</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Art Composition</div>
+                            <div class="item-case">View Details</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="item design">
+                  <a href="hitman.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/hitman/hitman-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Hitman Poster</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Art Composition</div>
+                            <div class="item-case">View Details</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="item wide web">
+                  <a href="elscorcho-salsa.php" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="/assets/img/projects/elscorcho-salsa/elscorcho-salsa-thumb.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Elscorcho Salsa</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Motion Graphics</div>
+                            <div class="item-case"><i class="fa fa-play-circle-o" aria-hidden="true"></i> Watch Video</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="item wide branding">
+                  <a href="project07.html" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="images/7.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Bruni Fashion</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Branding</div>
+                            <div class="item-case">See Case Study</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="item design">
+                  <a href="project08.html" class="ajax-link" data-type="page-transition">
+                    <div class="item-content">
+                      <div class="item-image" data-src="images/8.jpg"></div>
+                      <div class="item-caption">
+                        <div class="item-overlay">
+                          <div class="item-title">Yomagick</div>
+                          <div class="item-sub-mask">
+                            <div class="item-cat">Art Direction</div>
+                            <div class="item-case">See Case Study</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+
+              </div>
+              <!--/Portfolio -->
 
             </div>
+            <!--/Portfolio Wrap -->
+
+
+          </div>
         </div>
         <!--/Main -->
 
@@ -227,8 +227,8 @@
         <footer class="hidden">
 
 
-	    <div id="page-action-holder" data-tooltip="View Filters" data-placement="right">
-                <div id="open-filters"><i class="fa fa-eye" aria-hidden="true"></i></div>
-            </div>
+          <div id="page-action-holder" data-tooltip="View Filters" data-placement="right">
+            <div id="open-filters"><i class="fa fa-eye" aria-hidden="true"></i></div>
+          </div>
 
-        <?php include_once('inc/footer.php'); ?>
+          <?php include_once('inc/footer.php'); ?>
